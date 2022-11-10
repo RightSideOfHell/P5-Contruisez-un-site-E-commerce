d7c9a35f164c16156e3d87a24873717edfa5c59b
RightSideOfHell <kellylloansi@gmail.com>
2022-11-10 20:57:48 +0100

    id ajoutée dans product.js

--- front/js/product.js
+++ front/js/product.js
@@ -5,7 +5,7 @@ let product_id = urlParam.get('id')
     console.log(product_id)
 
 
-fetch(`http://localhost:3000/api/products?id=${product_id}`)
+fetch(`http://localhost:3000/api/products/${product_id}`)
 .then(function(res) {
     if (res.ok) {
     return res.json();
@@ -17,11 +17,9 @@ fetch(`http://localhost:3000/api/products?id=${product_id}`)
 })
 
 
-//Afficher les données  
-
-/*function showProducts(products){
-    for (product of products) {
-        let presentationImg = document.querySelector(".item__img")
+//Afficher les données
+function showProducts(product) {
+    let presentationImg = document.querySelector(".item__img")
         let image = document.createElement("img");
         image.src = `${product.imageUrl}`
         image.alt = `${product.altTxt}`;
@@ -32,14 +30,4 @@ fetch(`http://localhost:3000/api/products?id=${product_id}`)
         presentationPrice.textContent= `${product.price}`
         let presentationDescription = document.querySelector("#description")
         presentationDescription.textContent= product.description
-console.log(product.imageUrl)
-console.log(product.name)
-    }
-}*/
-
-/*function showProducts(product) {
-    let presentationImg = document.querySelector(".item__img");
-    let image = document.createElement("img");
-    presentationImg.appendChild(image);
-    //image.src = product.imageUrl;
-}*/
+}
