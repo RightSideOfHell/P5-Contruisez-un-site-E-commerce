5c07d194a80c2506d7b313393447bbf721168cff
RightSideOfHell <kellylloansi@gmail.com>
2022-11-17 18:24:38 +0100

    ajout option de couleur

--- front/js/product.js
+++ front/js/product.js
@@ -2,7 +2,7 @@
 let str = window.location.search
 let urlParam = new URLSearchParams(str)
 let product_id = urlParam.get('id')
-    console.log(product_id)
+    //console.log(product_id)
 
 
 fetch(`http://localhost:3000/api/products/${product_id}`)
@@ -13,7 +13,7 @@ fetch(`http://localhost:3000/api/products/${product_id}`)
 })
 .then(function(value) {
     showProducts(value)
-    console.log("Sucess:", value);
+    //console.log("Sucess:", value);
 })
 
 
@@ -21,13 +21,35 @@ fetch(`http://localhost:3000/api/products/${product_id}`)
 function showProducts(product) {
     let presentationImg = document.querySelector(".item__img")
         let image = document.createElement("img");
-        image.src = `${product.imageUrl}`
-        image.alt = `${product.altTxt}`;
+        image.src = product.imageUrl
+        image.alt = product.altTxt
         presentationImg.appendChild(image);
-        let presentationTitle = document.querySelector("#title")
+    let presentationTitle = document.querySelector("#title")
         presentationTitle.textContent= product.name
-        let presentationPrice = document.querySelector("#price")
-        presentationPrice.textContent= `${product.price}`
-        let presentationDescription = document.querySelector("#description")
+    let presentationPrice = document.querySelector("#price")
+        presentationPrice.textContent= product.price
+    let presentationDescription = document.querySelector("#description")
         presentationDescription.textContent= product.description
+//Add colors// 
+    let optionDiv = document.querySelector("#colors")
+    let colorData = product.colors
+    for(color of colorData) {
+        let option = document.createElement("option")
+        option.setAttribute("value", color)
+        option.textContent = color
+
+        optionDiv.appendChild(option)
+    }          
+}
+
+//local storage
+
+let addToCart = document.querySelector("#addToCart")
+function addProductToCart(){
+    addToCart.addEventListener('click', () => {
+        let color = document.querySelector("#colors").value
+        let qtty = document.querySelector("#quantity").value 
+        let productArray = [product_id, color, qtty ]
+        localStorage.setItem('panier', productArray)
+    })
 }
