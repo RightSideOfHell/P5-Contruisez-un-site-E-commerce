a1b46bd4761fb8aab8f8e5f078946c6c2ace107f
RightSideOfHell <kellylloansi@gmail.com>
2022-11-22 14:01:28 +0100

    ajout des valeurs additionnées de canapé dans le local storage

--- front/js/product.js
+++ front/js/product.js
@@ -2,7 +2,6 @@
 let str = window.location.search
 let urlParam = new URLSearchParams(str)
 let product_id = urlParam.get('id')
-    //console.log(product_id)
 
 
 fetch(`http://localhost:3000/api/products/${product_id}`)
@@ -13,7 +12,6 @@ fetch(`http://localhost:3000/api/products/${product_id}`)
 })
 .then(function(value) {
     showProducts(value)
-    //console.log("Sucess:", value);
 })
 
 
@@ -42,14 +40,129 @@ function showProducts(product) {
     }          
 }
 
-//local storage
 
-let addToCart = document.querySelector("#addToCart")
-function addProductToCart(){
-    addToCart.addEventListener('click', () => {
-        let color = document.querySelector("#colors").value
-        let qtty = document.querySelector("#quantity").value 
-        let productArray = [product_id, color, qtty ]
-        localStorage.setItem('panier', productArray)
-    })
+//local storage
+let cart = document.querySelector("#addToCart")
+cart.addEventListener('click', products)
+
+function products(){
+    let color = document.querySelector("#colors").value
+    let qtty = document.querySelector("#quantity").value 
+    let product = [product_id, color, qtty]
+    let productKey = product_id + color
+
+    if(localStorage.getItem(productKey)){
+        let quantiteInitiale = localStorage.getItem(productKey)
+        let quantiteAdded = parseInt(quantiteInitiale) + parseInt(qtty)
+        localStorage.setItem(product_id + color, quantiteAdded)
+    } else {
+        localStorage.setItem(product_id + color, qtty)
+    }
 }
+
+//if product already here: XX -> else if id but no color: XXX -> else if id + color but no qty: XXXX  -> else: localstorage id + color + qtty
+
+
+
+
+
+/*    
+setItems(product)
+    //let productNumbers = localStorage.getItem(product)
+    //productNumbers = parseInt(productNumbers)
+
+function setItems(product){
+    let quantiteInitiale = JSON.parse(localStorage.getItem(productKey))
+    console.log("base", quantiteInitiale)
+    console.log(typeof quantiteInitiale)
+}
+
+if(localStorage.getItem(productKey)){
+    let quantiteAjoute = quantiteInitiale[0].value
+    console.log("quantité ajouté", quantiteAjoute)
+
+}
+*/
+
+/*
+    let currentqtty = localStorage.setItem(product_id + color, qtty)
+    console.log("currentqtty", currentqtty)
+    let newcurrentqtty = parseInt(currentqtty + baseqtty)
+    console.log("final qtty", newcurrentqtty)
+    console.log(typeof newcurrentqtty)
+    if (productNumbers) {
+        localStorage.setItem(product_id + color, newcurrentqtty) //+ l'anciennce value)
+    }else {
+        localStorage.setItem(product_id + color, qtty)
+    }
+
+*/
+/*
+    //let parsedProduct = parseInt(product)
+    //console.log(typeof parsedProduct)
+
+checkItem(product)
+
+let baseqtty = localStorage.getItem(qtty)
+    console.log("base", product[2])
+
+
+/*
+function checkItem(product) {
+    let item = localStorage.getItem(parsedProduct)
+    console.log("item", item)
+}
+*/
+/*
+    //let productNumbers = localStorage.getItem(product)
+    //newProductNumbers = parseInt(productNumbers)
+    //console.log(newProductNumbers)
+    //console.log(typeof newProductNumbers)
+
+    let baseqtty = localStorage.getItem(qtty)
+    console.log("base", product[2])
+    let currentqtty = localStorage.setItem(product_id + color, qtty)
+    console.log("currentqtty", currentqtty)
+    let newcurrentqtty = parseInt(currentqtty + baseqtty)
+    console.log("final qtty", newcurrentqtty)
+    console.log(typeof newcurrentqtty)
+    if (productNumbers) {
+        localStorage.setItem(product_id + color, newcurrentqtty) //+ l'anciennce value)
+    }else {
+        localStorage.setItem(product_id + color, qtty)
+    }
+*/
+
+
+    //let returnedProduct = JSON.parse(localStorage.getItem(tessst))
+    //console.log("pls work?", returnedProduct)
+
+
+
+/*    
+function returnProduct() {
+    let productNumbers = localStorage.getItem('returnProduct')
+    localStorage.setItem(productArray[0] + productArray[1], productArray[2])
+    let basket = JSON.parse(localStorage.getItem(seToLocal))
+    return (basket)
+}
+
+    //localStorage.setItem(productArray[0] + productArray[1], productArray[2])
+
+
+    localStorage.setItem(productArray[0] + productArray[1], productArray[2])
+    productArray = parseInt(productArray)
+    console.log(typeof productArray)
+    if (productArray){
+        localStorage.setItem(productArray[0] + productArray[1], productArray[2] +1)
+    }else {
+        localStorage.setItem(productArray[0] + productArray[1], productArray[2])
+    }   
+
+    
+    function cartNumbers() {
+        let basket = JSON.parse(localStorage.getItem(addToCart))
+        return (basket)
+        console.log(basket)
+    }  
+*/
